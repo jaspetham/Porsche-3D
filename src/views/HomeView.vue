51ad73d02a5876655115a8229dcdba29b12f5e36
jaspetham <jaspertham98@gmail.com>
2025-04-01 16:27:36 +0800

    update hero scene path

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import AboutSection from '@/components/AboutSection.vue'
-import HeaderSection from '@/components/HeaderSection.vue'
-import HeroSection from '@/components/HeroSection.vue'
-import ThreeScene from '@/components/Scene/ThreeScene.vue'
-import ScreenLoader from '@/components/ScreenLoader.vue'
+import AboutSection from "@/components/AboutSection.vue";
+import HeaderSection from "@/components/HeaderSection.vue";
+import HeroSection from "@/components/HeroSection.vue";
+import HeroScene from "@/components/Scene/HeroScene.vue";
+import ScreenLoader from "@/components/ScreenLoader.vue";
 </script>
 
 <template>
   <ScreenLoader />
   <HeaderSection />
-  <ThreeScene />
+  <HeroScene />
   <!-- <main id="main-container" class="show"> -->
   <main id="main-container">
     <HeroSection />
