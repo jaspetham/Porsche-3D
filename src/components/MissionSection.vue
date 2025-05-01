01d403ead1225ab2cba89ae3d4d649fa6cf3916a
jaspetham <jaspertham98@gmail.com>
2025-05-02 02:42:05 +0800

    test mission

--- src/components/MissionSection.vue
+++ src/components/MissionSection.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts"></script>
 <template>
-  <section id="mission" class="h-[200vh] w-screen"></section>
+  <section id="mission" class="w-screen relative overflow-y-auto">
+    <div class="grid grid-cols-2">
+      <div class="mission-bg sticky top-0 h-[100dvh]">
+        <img
+          src="/assets/mission/founder.avif"
+          alt="mission-bg"
+          class="w-full h-full object-cover aspect-video"
+        />
+      </div>
+      <div class="mission-desc h-[200dvh] bg-red-500"></div>
+    </div>
+  </section>
 </template>
 <style lang="scss"></style>
